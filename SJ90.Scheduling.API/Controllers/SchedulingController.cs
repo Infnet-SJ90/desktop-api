f3aa0da4b2afa8fedf65713523be8e6bf6e72f8a
Yan Alves <yanalves@hotmail.com>
2018-07-22 17:25:21 -0300

    :tada: Adiciona primeira camada da aplicação, com documentação XML e Swagger

---
+++ SJ90.Scheduling.API/Controllers/SchedulingController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SJ90.Scheduling.API.Controllers
+{
+    [Route("v1/[controller]")]
+    public class SchedulingController : Controller
+    {
+        /// <summary>
+        /// Obtém todos os agendamentos
+        /// </summary>
+        /// <returns>Todos os agendamentos cadastrados</returns>
+        [HttpGet]
+        public IEnumerable<string> GetAll()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Obtém agendamento por identificador
+        /// </summary>
+        /// <param name="id">identificador do agendamento</param>
+        /// <returns>Agendamento com o identificador passado</returns>
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Adiciona um agendamento
+        /// </summary>
+        /// <param name="value">Agendamento a ser adicionado</param>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Atualiza um agendamento
+        /// </summary>
+        /// <param name="id">Identificador do agendamento</param>
+        /// <param name="value">Informações a serem atualizadas</param>
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Deleta um agendamento
+        /// </summary>
+        /// <param name="id">Identificador do agendamento a ser deletado</param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SJ90.Scheduling.API/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SJ90.Scheduling.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Gerenciador de agendamento",
+                        Version = "v1",
+                        Description = "API REST criada com o ASP.NET Core para lidar com configurações e gerencimento de agendamentos",
+                        Contact = new Contact
+                        {
+                            Name = "SJ90",
+                            Url = "https://github.com/Infnet-SJ90"
+                        }
+                    });
+
+                    string applicationBasePath =
+                        PlatformServices.Default.Application.ApplicationBasePath;
+                    string applicationName =
+                        PlatformServices.Default.Application.ApplicationName;
+                    string xmlDocPath =
+                        Path.Combine(applicationBasePath, $"{applicationName}.xml");
+
+                    c.IncludeXmlComments(xmlDocPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "Gerenciador de agendamentos");
+            });
+        }
+    }
+}
